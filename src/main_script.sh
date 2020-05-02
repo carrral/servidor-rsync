68a3c4da54ac3022cd257c6030d7f9a6d45f5efc
Carlos Carral <carlos-II@localhost.localdomain>
2020-05-02 00:04:00 -0500

    Ahora el programa unicamente lee un .txt de archivos a incluir. De este archivo obtiene un archivo secundario de archivos a excluir que es el que efectivamente lee rsync.

--- src/main_script.sh
+++ src/main_script.sh
@@ -1,25 +1,32 @@
 #!/bin/sh
 
 
-EXCLUDE_FILE=~/.config/servidor_rsync/.servidor_rsync
-DUMP_FILE=~/.config/.servidor_historial
+INCLUDE_FILE=~/.config/servidor_rsync/servidor_incluir
+EXCLUDE_FILE=~/.config/servidor_rsync/servidor_rsync_excluir
+DUMP_FILE=~/.config/servidor_rsync/servidor_historial
 BACKUP_FROM=carlos@192.168.100.51
 BACKUP_DIR=~/.home_backup
 SNAPSHOT_FNAME=~/.config/servidor_rsync/.servidor_rsync_snapshot
 MAIN_DIR=~/.config/servidor_historial
 
+
+
+## Get files to be excluded
+COMMAND="ls ~ "
+for dir in $(strings dirs.txt --encoding=S)
+do
+    COMMAND+="--ignore=\"${dir}\" "
+done
+
+eval $COMMAND > $EXCLUDE_FILE
+
 # Para ser corrido desde la computadora destino
 touch $DUMP_FILE
 echo "Última sincronización:" >  $DUMP_FILE
 echo $(date) | tee -a $DUMP_FILE
-echo "Directorios excluidos:" | tee -a $DUMP_FILE
 
-for d in $(strings $EXCLUDE_FILE)
-do
-    echo $d | tee -a $DUMP_FILE
-done
 
-rsync -uaPz --delete  --exclude-from $EXCLUDE_FILE  -e ssh $BACKUP_FROM:~/. /home/carlos/ | tee -a $DUMP_FILE || echo "Error al intentar sincronizar"
+rsync -uaPz --delete --exclude-from $EXCLUDE_FILE  -e ssh $BACKUP_FROM:~/. /home/carlos/ | tee -a $DUMP_FILE || echo "Error al intentar sincronizar"
 
 # Comprimir backup gradual
 
